089c4e76e71e099714b0c26e929a3c462ffa237d
Varun <varunkuamr.saini06@gmail.com>
2013-10-26 07:52:55 -0500

    fixed google analytics

--- public/javascripts/analytics.js
+++ public/javascripts/analytics.js
@@ -1,7 +1,7 @@
 /**
  * Created by Varun on 10/26/13.
  */
-<script>
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -10,4 +10,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-42065254-2', 'popularwebtech.herokuapp.com');
 ga('send', 'pageview');
 
-</script>
